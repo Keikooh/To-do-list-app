e4116f5d262982cf90390be7936b128c350d9615
fer <fergzm05@gmail.com>
2022-10-08 15:30:14 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 
-import { saludar } from './js/componentes.js';
 import './styles.css';
 
-const nombre = 'Fernandaa';
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-saludar( nombre );
+// import { Todo } from './classes/todo.class';
+// import { TodoList } from './classes/todo-list-class';
+
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach( todo => crearTodoHtml(todo));
+
+// const newTodo = new Todo('Aprender javascript');
+// todoList.nuevoTodo(newTodo);
+
+console.log('todos', todoList.todos);
+
